fb34d215940338b7a33037a01866a5257d7b8901
breachr <brianhoang873@gmail.com>
2024-10-16 02:28:15 -0700

    Set up VPC, public and private subnets, route tables, route table associations, and internet gateway

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ docker-compose down
 ### Getting Started with Terraform
 This guide provides step-by-step instructions to help you get started using Terraform with AWS to manage your cloud infrastructure.
 
-1. Install [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)
+1. Install [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html)<br>
 After installing the AWS CLI, you need to configure it with your credentials using your access key and secret key:
 ```
 aws configure
@@ -45,15 +45,18 @@ aws configure
 ```
 cd infrastructure
 ```
-2. Initalize Terraform, to install the necessary provider plugins
+2. Initalize Terraform <br>
+This command will install the necessary provider plugins, as defined in the configuration file:
 ```
 terraform init
 ```
-3. Apply Infrastructure Changes
+3. Apply Infrastructure Changes <br>
+This will create/update the resources on your AWS account:
 ```
-terraform plan
+terraform apply
 ```
-4. Destroy Resources
+4. Destroy Resources <br>
+To remove the infrastructure when you're done run the command:
 ```
 terraform destroy
 ```
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,3 +15,55 @@ provider "aws" {
 resource "aws_vpc" "app_network" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "public-subnet" {
+  vpc_id = aws_vpc.app_network.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private-subnet" {
+  vpc_id = aws_vpc.app_network.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "vpc-internet-gateway" {
+  vpc_id = aws_vpc.app_network.id
+
+  tags = {
+    Name = "vpc-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.app_network.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.app_network.id
+  
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public-route-table-association" {
+  subnet_id      = aws_subnet.public-subnet.id
+  route_table_id = aws_route_table.public-route-table.id
+}
+
+resource "aws_route_table_association" "private-route-table-association" {
+  subnet_id      = aws_subnet.private-subnet.id
+  route_table_id = aws_route_table.private-route-table.id
+}
