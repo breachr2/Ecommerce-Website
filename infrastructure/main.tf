4402b3c59e1dd9a49ef49c2dfec2c6ab163db71e
breachr <brianhoang873@gmail.com>
2024-10-16 01:54:37 -0700

    Set up vpc and updated readme

---
+++ infrastructure/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_vpc" "app_network" {
+  cidr_block = "10.0.0.0/16"
+}
