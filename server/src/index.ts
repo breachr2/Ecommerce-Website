b9a1d5a9b6cf6f073aa3593722bb94cd83714578
breachr <brianhoang873@gmail.com>
2024-10-20 15:48:13 -0700

    Set up backend with Docker and connected it to database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,23 +1,31 @@
+
 import express from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-
 dotenv.config();
 const app = express();
+import { PrismaClient } from "@prisma/client";
+
 app.use(express.json());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+const prisma = new PrismaClient();
 
 const port = process.env.PORT || 8000;
 
-app.get("/hello", (req, res) => {
-  res.send("Hello");
+app.get("/api/products", async (req, res) => {
+  const result = await prisma.products.findMany();
+  res.json(result)
+});
+
+app.get("/", (req, res) => {
+  res.json("hello15151")
 });
 
 app.listen(port, () => {
