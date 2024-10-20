fb59146b053db15bac9fb8c7bc6f7d5c6220675f
breachr <brianhoang873@gmail.com>
2024-10-20 16:37:25 -0700

    Set up docker with frontend and connected it to backend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,19 @@
 import Image from "next/image";
-import Dashboard from "@/app/dashboard/page"
+import Dashboard from "@/app/dashboard/page";
 
-export default function Home() {
-  return <Dashboard />;
+export default async function Home() {
+  // Testing fetch call to backend
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)
+  const products = await data.json()
+  const product1 = products[11]
+  return (
+    <>
+      <div>
+        <p>{product1.productId}</p>
+        <p>{product1.name}</p>
+        <p>{product1.price}</p>
+      </div>
+      <Dashboard />
+    </>
+  );
 }
