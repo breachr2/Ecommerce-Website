6b28b9006e9157c3f5296e3940197f8f70f5ddab
frankllama <chewfasa@gmail.com>
2024-11-09 17:51:32 -0800

    Make api calls from front end to back end auto instead of manually.

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,5 +1,12 @@
-export default function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
-}
+// Default that was created
+// export default function Dashboard() {
+//   return (
+//     <div>Dashboard</div>
+//   )
+// }
+
+const Dashboard = () => {
+  return <div>Dashboard</div>;
+};
+
+export default Dashboard;
