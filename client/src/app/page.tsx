fb34cf629943023f2658b9da67b745093b9e073e
frankllama <chewfasa@gmail.com>
2024-11-16 18:35:05 -0800

    Added line to fix bug with client side display of app using Linux, need to uncomment or comment for Windows. Started to work on the dashboard page layout and styling. Created new file for Popular Products card.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,10 @@ import Dashboard from "@/app/dashboard/page";
 
 export default async function Home() {
   // Testing fetch call to backend
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)
+  // Uncomment below for Windows version. Due to path directory separator differences.
+  //const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)
+  // Uncomment below for Linux version. Due to path directory separator differences.
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}api/products`)
   const products = await data.json()
   const product1 = products[0]
   return (
