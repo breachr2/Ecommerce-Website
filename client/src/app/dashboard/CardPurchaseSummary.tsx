d49eaccbdcbb5e5a9fa67748febc283399034877
frankllama <chewfasa@gmail.com>
2024-11-30 18:30:28 -0800

    Finished creating dashboard card for card purchase summary. Fixed recharts graphics not displaying by updating recharts npm package in client directory.

--- client/src/app/dashboard/CardPurchaseSummary.tsx
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -2,6 +2,7 @@ import { useGetDashboardMetricsQuery } from '@/state/api';
 import { TrendingDown, TrendingUp } from 'lucide-react';
 import numeral from 'numeral';
 import React from 'react';
+import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const CardPurchaseSummary = () => {
   const { data, isLoading } = useGetDashboardMetricsQuery();
@@ -24,6 +25,7 @@ const CardPurchaseSummary = () => {
 
           {/* BODY */}
           <div>
+            {/* Body Header */ }
             <div className="mb-4 mt-7 px-7">
               <p className="text-xs text-gray-400">Purchased</p>
               <div className="flex items-center">
@@ -32,24 +34,54 @@ const CardPurchaseSummary = () => {
                   ? numeral(lastDataPoint.totalPurchased).format("$0.00a")
                   : "0"}
                 </p>
+                {lastDataPoint && (
+                  <p 
+                    className={`text-sm ${ 
+                      lastDataPoint.changePercentage! >= 0 
+                        ? "text-green-500"
+                        : "text-red-500"
+                    } flex ml-3`}
+                  >
+                    {lastDataPoint.changePercentage! >= 0 ? (
+                      <TrendingUp className="w-5 h-5 mr-1" /> 
+                    ) : (
+                      <TrendingDown className="w-5 h-5 mr-1" />
+                    )}
+                    {Math.abs(lastDataPoint.changePercentage!)}%
+                  </p>
+                )}
               </div>
-              {lastDataPoint && (
-                <p 
-                  className={`text-sm ${ 
-                    lastDataPoint.changePercentage! >= 0 
-                      ? "text-green-500"
-                      : "text-red-500"
-                  } flex ml-3`}
-                >
-                  {lastDataPoint.changePercentage! >= 0 ? (
-                    <TrendingUp className="w-4 h-4" /> 
-                  ) : (
-                    <TrendingDown className="w-4 h-4" />
-                  )}
-                  {Math.abs(lastDataPoint.changePercentage!)}%
-                </p>
-              )}
             </div>
+            {/* Chart */}
+            <ResponsiveContainer width="100%" height={200} className="p-2">
+              <AreaChart
+                data={purchaseData}
+                margin={{ top: 0, right: 0, left: -50, bottom: 45 }}
+              >
+                <XAxis dataKey="date" tick={false} axisLine={false} />
+                <YAxis tickLine={false} tick={false} axisLine={false} />
+                <Tooltip
+                  formatter={(value: number) => [
+                    `$${value.toLocaleString("en")}`,
+                  ]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
+                />
+                <Area
+                  type="linear"
+                  dataKey="totalPurchased"
+                  stroke="#8884d8"
+                  fill="#8884d8"
+                  dot={true}
+                />
+              </AreaChart>
+            </ResponsiveContainer>
           </div>
         </>
       )}
