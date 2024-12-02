98194d6e5ff9f1bcf525fd85e7fc41110df48e4a
breachr <brianhoang873@gmail.com>
2024-12-02 12:34:18 -0800

    Update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Dashboard from "@/app/dashboard/page";
 
 export default async function Home() {
@@ -6,16 +5,11 @@ export default async function Home() {
   // Uncomment below for Windows version. Due to path directory separator differences.
   //const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)
   // Uncomment below for Linux version. Due to path directory separator differences.
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)
-  const products = await data.json()
-  const product1 = products[0]
+  // const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)
+  // const products = await data.json()
+  // const product1 = products[0]
   return (
     <>
-      <div>
-        <p>{product1.productId}</p>
-        <p>{product1.name}</p>
-        <p>{product1.price}</p>
-      </div>
       <Dashboard />
     </>
   );
