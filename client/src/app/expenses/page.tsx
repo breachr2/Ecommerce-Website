c2bca910136f02e26bf1b0740868974e7fe2b1c2
breachr <brianhoang873@gmail.com>
2024-12-02 12:02:56 -0800

    Added expenses page

---
+++ client/src/app/expenses/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useGetExpensesByCategoryQuery } from "@/state/api";
+import { useMemo, useState } from "react";
+import Header from "../(components)/Header";
+
+const Expenses = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+
+  const {
+    data: expensesData,
+    isLoading,
+    isError,
+  } = useGetExpensesByCategoryQuery();
+  const expenses = useMemo(() => expensesData ?? [], [expensesData]);
+
+  if (isLoading) {
+    return <div className="py-4">Loading...</div>;
+  }
+
+  if (isError || !expensesData) {
+    return (
+      <div className="text-center text-red-500 py-4">
+        Failed to fetch expenses
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="mb-5">
+        <Header name="Expenses" />
+        <p className="text-sm text-gray-500">
+          A visual representation of expenses over time.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Expenses;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   // Uncomment below for Windows version. Due to path directory separator differences.
   //const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)
   // Uncomment below for Linux version. Due to path directory separator differences.
-  const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}api/products`)
+  const data = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)
   const products = await data.json()
   const product1 = products[0]
   return (
